a10610d89f18f25d7c622ae5af3938805c1eae55
ChaeLin <evergreen786@naver.com>
2020-02-12 15:38:48 +0900

    dd

--- rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/server/AWSConnection.java
+++ rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/server/AWSConnection.java
@@ -38,54 +38,54 @@ public class AWSConnection {
 //                }
 //            }
 //        );
-        if(transferUtility == null) {
-            transferUtility =
-                    TransferUtility.builder()
-                            .context(context)
-                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
-                            .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentials()))
-                            .build();
-        }
-
-        TransferObserver uploadObserver =
-                transferUtility.upload(
-                        "public/"+fileName,
-                        new File(localPath));
-
-        // Attach a listener to the observer to get state update and progress notifications
-        uploadObserver.setTransferListener(new TransferListener() {
-
-            @Override
-            public void onStateChanged(int id, TransferState state) {
-                if (TransferState.COMPLETED == state) {
-                    // Handle a completed upload.
-                }
-            }
-
-            @Override
-            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
-                float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
-                int percentDone = (int)percentDonef;
-
-                Log.d("YourActivity", "ID:" + id + " bytesCurrent: " + bytesCurrent
-                        + " bytesTotal: " + bytesTotal + " " + percentDone + "%");
-            }
-
-            @Override
-            public void onError(int id, Exception ex) {
-                // Handle errors
-                Log.e("error", ""+ ex.getMessage());
-            }
-        });
-
-        // If you prefer to poll for the data, instead of attaching a
-        // listener, check for the state and progress in the observer.
-        if (TransferState.COMPLETED == uploadObserver.getState()) {
-            // Handle a completed upload.
-        }
-
-        Log.d("YourActivity", "Bytes Transferred: " + uploadObserver.getBytesTransferred());
-        Log.d("YourActivity", "Bytes Total: " + uploadObserver.getBytesTotal());
+//        if(transferUtility == null) {
+//            transferUtility =
+//                    TransferUtility.builder()
+//                            .context(context)
+//                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+//                            .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentials()))
+//                            .build();
+//        }
+//
+//        TransferObserver uploadObserver =
+//                transferUtility.upload(
+//                        "public/"+fileName,
+//                        new File(localPath));
+//
+//        // Attach a listener to the observer to get state update and progress notifications
+//        uploadObserver.setTransferListener(new TransferListener() {
+//
+//            @Override
+//            public void onStateChanged(int id, TransferState state) {
+//                if (TransferState.COMPLETED == state) {
+//                    // Handle a completed upload.
+//                }
+//            }
+//
+//            @Override
+//            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+//                float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
+//                int percentDone = (int)percentDonef;
+//
+//                Log.d("YourActivity", "ID:" + id + " bytesCurrent: " + bytesCurrent
+//                        + " bytesTotal: " + bytesTotal + " " + percentDone + "%");
+//            }
+//
+//            @Override
+//            public void onError(int id, Exception ex) {
+//                // Handle errors
+//                Log.e("error", ""+ ex.getMessage());
+//            }
+//        });
+//
+//        // If you prefer to poll for the data, instead of attaching a
+//        // listener, check for the state and progress in the observer.
+//        if (TransferState.COMPLETED == uploadObserver.getState()) {
+//            // Handle a completed upload.
+//        }
+//
+//        Log.d("YourActivity", "Bytes Transferred: " + uploadObserver.getBytesTransferred());
+//        Log.d("YourActivity", "Bytes Total: " + uploadObserver.getBytesTotal());
     }
 
     public static void downloadFile(String fileName, String localPath, Context context) {
@@ -107,60 +107,60 @@ public class AWSConnection {
 
         //String localPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/chatDown/subinfo.txt";
 
-        if(transferUtility == null) {
-            transferUtility =
-                    TransferUtility.builder()
-                            .context(context)
-                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
-                            .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentials()))
-                            .build();
-        }
-
-
-        String key = "public/test/"+fileName+".txt";
-        Log.d("PKR","download"+key);
-
-        TransferObserver downloadObserver =
-                transferUtility.download(
-                        key,
-                        new File(localPath));
-
-        // Attach a listener to the observer to get notified of the
-        // updates in the state and the progress
-        downloadObserver.setTransferListener(new TransferListener() {
-
-            @Override
-            public void onStateChanged(int id, TransferState state) {
-                if (TransferState.COMPLETED == state) {
-                    // Handle a completed upload.
-                    awsListner.downloadComplete();
-                }
-            }
-
-            @Override
-            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
-                float percentDonef = ((float)bytesCurrent/(float)bytesTotal) * 100;
-                int percentDone = (int)percentDonef;
-
-                Log.d("MainActivity", "   ID:" + id + "   bytesCurrent: " + bytesCurrent + "   bytesTotal: " + bytesTotal + " " + percentDone + "%");
-            }
-
-            @Override
-            public void onError(int id, Exception ex) {
-                // Handle errors
-            }
-
-        });
-
-        // If you do not want to attach a listener and poll for the data
-        // from the observer, you can check for the state and the progress
-        // in the observer.
-        if (TransferState.COMPLETED == downloadObserver.getState()) {
-            // Handle a completed upload.
-        }
-
-        Log.d("YourActivity", "Bytes Transferred: " + downloadObserver.getBytesTransferred());
-        Log.d("YourActivity", "Bytes Total: " + downloadObserver.getBytesTotal());
+//        if(transferUtility == null) {
+//            transferUtility =
+//                    TransferUtility.builder()
+//                            .context(context)
+//                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+//                            .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentials()))
+//                            .build();
+//        }
+//
+//
+//        String key = "public/test/"+fileName+".txt";
+//        Log.d("PKR","download"+key);
+//
+//        TransferObserver downloadObserver =
+//                transferUtility.download(
+//                        key,
+//                        new File(localPath));
+//
+//        // Attach a listener to the observer to get notified of the
+//        // updates in the state and the progress
+//        downloadObserver.setTransferListener(new TransferListener() {
+//
+//            @Override
+//            public void onStateChanged(int id, TransferState state) {
+//                if (TransferState.COMPLETED == state) {
+//                    // Handle a completed upload.
+//                    awsListner.downloadComplete();
+//                }
+//            }
+//
+//            @Override
+//            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+//                float percentDonef = ((float)bytesCurrent/(float)bytesTotal) * 100;
+//                int percentDone = (int)percentDonef;
+//
+//                Log.d("MainActivity", "   ID:" + id + "   bytesCurrent: " + bytesCurrent + "   bytesTotal: " + bytesTotal + " " + percentDone + "%");
+//            }
+//
+//            @Override
+//            public void onError(int id, Exception ex) {
+//                // Handle errors
+//            }
+//
+//        });
+//
+//        // If you do not want to attach a listener and poll for the data
+//        // from the observer, you can check for the state and the progress
+//        // in the observer.
+//        if (TransferState.COMPLETED == downloadObserver.getState()) {
+//            // Handle a completed upload.
+//        }
+//
+//        Log.d("YourActivity", "Bytes Transferred: " + downloadObserver.getBytesTransferred());
+//        Log.d("YourActivity", "Bytes Total: " + downloadObserver.getBytesTotal());
     }
 
 //    public static void downloadFile(String fileName, String localPath){
