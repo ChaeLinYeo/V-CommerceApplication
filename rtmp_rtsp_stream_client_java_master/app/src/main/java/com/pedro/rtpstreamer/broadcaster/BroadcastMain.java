553ed83d7fb89d8a6c108ce85b91a49400238e7e
ChaeLin <evergreen786@naver.com>
2020-02-12 12:03:35 +0900

    수정

--- rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/broadcaster/BroadcastMain.java
+++ rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/broadcaster/BroadcastMain.java
@@ -246,50 +246,7 @@ public class BroadcastMain extends AppCompatActivity
                 break;
         }
     }
-
-    public Bitmap loadBitmap(String url)
-    {
-        Bitmap bm = null;
-        InputStream is = null;
-        BufferedInputStream bis = null;
-        try
-        {
-            URLConnection conn = new URL(url).openConnection();
-            conn.connect();
-            is = conn.getInputStream();
-            bis = new BufferedInputStream(is, 8192);
-            bm = BitmapFactory.decodeStream(bis);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        finally {
-            if (bis != null)
-            {
-                try
-                {
-                    bis.close();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-            if (is != null)
-            {
-                try
-                {
-                    is.close();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return bm;
-    }
+    
 
     //For Broadcast info
     public View.OnClickListener broadcastClickListner = (View view) -> {
