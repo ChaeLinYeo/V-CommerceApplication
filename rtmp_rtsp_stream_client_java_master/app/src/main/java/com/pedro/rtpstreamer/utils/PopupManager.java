2137b7b51e352e3649f22f220c4fa129e95f865a
chocchic <dntksemfdj473@gmail.com>
2020-02-14 11:43:05 +0900

    event button text

--- rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/utils/PopupManager.java
+++ rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/utils/PopupManager.java
@@ -70,7 +70,7 @@ public class PopupManager {
     private boolean is_declare_6 = false;	//기타
     //신고 사유 기술용 변수
     private String txt_dummy_save;
-
+    Button makecoupon;
     //이벤트 쿠폰 팝업용 변수
     public String e_n = "";
     public String e_a="";
@@ -229,7 +229,11 @@ public class PopupManager {
                     e_t_m = numberpicker_m.getValue();
                     e_t_s = numberpicker_s.getValue();
                     alertDialog.dismiss();
-                }
+            if(e_a.equals("") || e_n.equals("") || (e_t_s < 1 && e_t_m < 1 && e_t_s < 1)){
+                makecoupon.setText("이벤트 설정");
+            }else{
+                makecoupon.setText("이벤트 수정");}
+            }
         );
 
         alertDialog.show();
@@ -260,9 +264,6 @@ public class PopupManager {
         coupon_name_txt.setText(e_n);
         coupon_ect_txt.setText(e_a);
 
-        //초*1000
-        //분*1000*60
-        //시*1000*60*60
         save_time_before = (e_t_h*1000*60*60) + (e_t_m*1000*60) + (e_t_s*1000); //시간 int로 저장
         save_time = e_t_h + e_t_m + e_t_s;
         alert01.setView(mView01);
@@ -319,7 +320,7 @@ public class PopupManager {
         Button selectAllButton = mView.findViewById(R.id.select_all);
         Button ban = mView.findViewById(R.id.ben);
         Button sendcoupon =  mView.findViewById(R.id.show_event);
-        Button makecoupon =  mView.findViewById(R.id.custom_event);
+        makecoupon =  mView.findViewById(R.id.custom_event);
         EditText search = mView.findViewById(R.id.searchPeople);
         SwipeRefreshLayout mSwipeRefreshLayout = mView.findViewById(R.id.swipeRefresh);
         mSwipeRefreshLayout.setOnRefreshListener(() -> {
@@ -390,9 +391,9 @@ public class PopupManager {
                 }
         );
 
-        makecoupon.setOnClickListener((View v) ->
-            btn_editPopUp(inflater)
-        );
+        makecoupon.setOnClickListener((View v) ->{
+            btn_editPopUp(inflater);
+        });
 
         sendcoupon.setOnClickListener((View v) -> {
                     if(e_a.equals("") || e_n.equals("") || (e_t_s < 1 && e_t_m < 1 && e_t_s < 1)){
@@ -412,6 +413,7 @@ public class PopupManager {
                         // 모든 선택 상태 초기화.
                         listview.clearChoices();
                         adapter.notifyDataSetChanged();
+                        makecoupon.setText("이벤트 설정");
                     }
                 }
         );
