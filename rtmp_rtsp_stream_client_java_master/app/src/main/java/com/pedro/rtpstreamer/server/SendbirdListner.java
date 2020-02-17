4c37c767a5a0c7001b3d99ae5a6026aa53e090ee
chocchic <dntksemfdj473@gmail.com>
2020-02-17 14:09:25 +0900

    alarm fix

--- rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/server/SendbirdListner.java
+++ rtmp_rtsp_stream_client_java_master/app/src/main/java/com/pedro/rtpstreamer/server/SendbirdListner.java
@@ -30,4 +30,5 @@ public abstract class SendbirdListner {
     public void onMessageReceived(String customType, String data){}
     public void loadInitialMessage(String type, String data){}
     public void onTitleChanged(String titleString){}
-}
+    public void userenter(String enterduser){}
+    }
